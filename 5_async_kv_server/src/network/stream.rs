6043dccab8165f72c8ef75104ac49bea48bf1dfe
KuanHsiaoKuo <kuanhsiaokuo@gmail.com>
2023-02-07 17:52:59 +0800

    async_kv_server: 针对异步做出的改造进行注释说明

--- 5_async_kv_server/src/network/stream.rs
+++ 5_async_kv_server/src/network/stream.rs
@@ -21,6 +21,9 @@ pub struct ProstStream<S, In, Out> {
     rbuf: BytesMut,
 
     // 类型占位符
+    // Rust 不允许数据结构有超出需要的泛型参数。
+    // 可以用 PhantomData，之前讲过它是一个零字节大小的占位符，
+    // 可以让我们的数据结构携带未使用的泛型参数。
     _in: PhantomData<In>,
     _out: PhantomData<Out>,
 }
@@ -43,6 +46,16 @@ where
 
         // 使用 read_frame 来获取数据
         let fut = read_frame(&mut self.stream, &mut rest);
+        // 因为 poll_xxx() 方法已经是 async/await 的底层 API 实现，
+        // 所以我们在 poll_xxx() 方法中，是不能直接使用异步函数的，
+        // 需要把它看作一个 future，然后调用 future 的 poll 函数。
+        // 因为 future 是一个 trait，所以需要 Box 将其处理成一个
+        // 在堆上的 trait object，这样就可以调用 FutureExt 的
+        // poll_unpin() 方法了。
+        // Box::pin 会生成 Pin。
+        // ready! 宏，
+        // 它会在 Pending 时直接 return Pending，
+        // 而在 Ready 时，返回 Ready 的值
         ready!(Box::pin(fut).poll_unpin(cx))?;
 
         // 拿到一个 frame 的数据，把 buffer 合并回去
@@ -63,10 +76,17 @@ where
     /// 如果发送出错，会返回 KvError
     type Error = KvError;
 
+    // 做背压的，你可以根据负载来决定要不要返回 Poll::Ready。
+    // 对于我们的网络层来说，可以先不关心背压，依靠操作系统的
+    // TCP 协议栈提供背压处理即可，所以这里直接返回 Poll::Ready(Ok(()))，
+    // 也就是说，上层想写数据，可以随时写
     fn poll_ready(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
     }
 
+    // 当 poll_ready() 返回 Ready 后，Sink 就走到 start_send()。
+    // 我们在 start_send() 里就把必要的数据准备好。这里把 item
+    // 封包成字节流，存入 wbuf 中
     fn start_send(self: Pin<&mut Self>, item: Out) -> Result<(), Self::Error> {
         let this = self.get_mut();
         item.encode_frame(&mut this.wbuf)?;
@@ -74,9 +94,13 @@ where
         Ok(())
     }
 
+    // 然后在 poll_flush() 中，我们开始写数据。
+    // 这里需要记录当前写到哪里，所以需要在
+    // ProstStream 里加一个字段 written，记录写入了多少字节
     fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         let this = self.get_mut();
 
+        // 有了这个 written 字段， 就可以循环写入
         // 循环写入 stream 中
         while this.written != this.wbuf.len() {
             let n = ready!(Pin::new(&mut this.stream).poll_write(cx, &this.wbuf[this.written..]))?;
@@ -92,6 +116,9 @@ where
         Poll::Ready(Ok(()))
     }
 
+    // 最后是 poll_close()，
+    // 我们只需要调用 stream 的 flush 和 shutdown 方法，
+    // 确保数据写完并且 stream 关闭
     fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         // 调用 stream 的 pull_flush 确保写入
         ready!(self.as_mut().poll_flush(cx))?;
@@ -102,8 +129,11 @@ where
     }
 }
 
-// 一般来说，如果我们的 Stream 是 Unpin，最好实现一下
 // Unpin 不像 Send/Sync 会自动实现
+// 一般来说，如果我们的 Stream 是 Unpin，最好实现一下
+// 这会给别人在使用你的代码时带来很多方便。
+// 一般来说，为异步操作而创建的数据结构，
+// 如果使用了泛型参数，那么只要内部没有自引用数据，就应该实现 Unpin。
 impl<S, In, Out> Unpin for ProstStream<S, In, Out> where S: Unpin {}
 
 impl<S, In, Out> ProstStream<S, In, Out>
